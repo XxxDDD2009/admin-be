a90a44daaf05655ad72584197ad4d17b7a246ffa
XxxDDD2009 <1096366539@qq.com>
2024-05-24 22:33:22 +0800

    init project with spring-boot 3.3.0

---
+++ src/main/java/com/xiyu/admin/AdminBeApplication.java
@@ -0,0 +1,13 @@
+package com.xiyu.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminBeApplication.class, args);
+    }
+
+}
